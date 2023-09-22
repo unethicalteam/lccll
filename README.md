bbaecf9463713a4026729b935064ffecb4fd4e94
Jason <38664452+uchks@users.noreply.github.com>
2023-09-22 03:55:54 -0500

    push base.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lccll
 lunar client command line launcher
+
+this is very much a work in progress.
+the current product is very much intended for my personal computer.
+
+roadmap:
+ - agent injection
+ - jre folder search / manual change
+ - jvm args
---
+++ lccll.bat
@@ -0,0 +1,87 @@
+@echo off
+setlocal enabledelayedexpansion
+set "LUNAR_VERSION=1.8.9"
+
+for /f "tokens=2 delims=: " %%a in ('curl -s https://launcherupdates.lunarclientcdn.com/latest.yml ^| findstr "version:"') do (
+    set "LAUNCHER_VERSION=%%a"
+)
+
+:menu
+cls
+echo   lccll - lunar client command line launcher
+echo   Created by [31munethical[0m
+echo   [36mhttps://discord.gg/vhJ8Dsp9qa[0m
+echo.
+if "%HWID%"=="0" (
+    echo   1^) Block HWID: Yes
+) else (
+    echo   1^) Block HWID: No
+)
+echo   2) Set Window Width and Height
+echo   3) Run Lunar Client
+echo   4) Exit
+echo.
+set /p "input=Enter the corresponding number and press Enter: "
+
+if "%input%"=="1" (
+    if "%HWID%"=="0" (
+        set "HWID_FILE=%USERPROFILE%\.lunarclient\launcher-cache\hwid-private-do-not-share"
+        if exist "%HWID_FILE%" (
+            set /p HWID=<"%HWID_FILE%"
+            goto menu
+        ) else (
+            echo HWID not found. HWID will remain blocked.
+        )
+    ) else (
+        set "HWID=0"
+        goto menu
+    )
+) else if "%input%"=="2" (
+    cls
+    set /p "LUNAR_WIDTH=Window width (e.g., 1920): "
+    set /p "LUNAR_HEIGHT=Window height (e.g., 1080): "
+) else if "%input%"=="3" (
+    cls
+    echo Running Lunar Client..
+    
+    set "LUNAR_JVM_ARGS=--add-modules jdk.naming.dns --add-exports jdk.naming.dns/com.sun.jndi.dns=java.naming -Djna.boot.library.path=natives -Dlog4j2.formatMsgNoLookups=true --add-opens java.base/java.io=ALL-UNNAMED -Xms3072m -Xmx3072m -Djava.library.path=natives"
+    set "LUNAR_JRE=%USERPROFILE%\.lunarclient\jre\4dcd188552ce8876d5e55e1f6d22505109bfa4cb\zulu17.34.19-ca-jre17.0.3-win_x64\bin\java"
+    set "LUNAR_CLASSPATH=lunar-lang.jar;lunar-emote.jar;lunar.jar;optifine-0.1.0-SNAPSHOT-all.jar;v1_8-0.1.0-SNAPSHOT-all.jar;common-0.1.0-SNAPSHOT-all.jar;genesis-0.1.0-SNAPSHOT-all.jar"
+    set "LUNAR_MAIN_CLASS=com.moonsworth.lunar.genesis.Genesis"
+    set "ICHOR_EXT_FILE=OptiFine_v1_8.jar"
+
+    cd "%USERPROFILE%\.lunarclient\offline\multiver"
+
+    %LUNAR_JRE% %LUNAR_JVM_ARGS% ^
+    	-cp "%LUNAR_CLASSPATH%" ^
+    	-javaagent:C:\Users\jason\.lunarclient\agents\Weave-Loader-Agent-0.2.4.jar ^
+    	-javaagent:C:\Users\jason\.lunarclient\agents\LunarAntiPollingRateCheck.jar ^
+    	-javaagent:C:\Users\jason\.lunarclient\agents\LunarEnable.jar ^
+    	"%LUNAR_MAIN_CLASS%" ^
+    	--version "%LUNAR_VERSION%" ^
+    	--accessToken 0 ^
+    	--assetIndex %LUNAR_VERSION:.9=% ^
+    	--userProperties {} ^
+    	--gameDir "%USERPROFILE%\.minecraft" ^
+    	--texturesDir "%USERPROFILE%\.lunarclient\textures" ^
+    	--width "%LUNAR_WIDTH%" ^
+    	--height "%LUNAR_HEIGHT%" ^
+    	--workingDirectory . ^
+    	--classpathDir . ^
+    	--ichorClassPath "%LUNAR_CLASSPATH%" ^
+    	--ichorExternalFiles "%ICHOR_EXT_FILE%" ^
+    	--installationId 0 ^
+    	--hwid %HWID% ^
+    	--launcherVersion %LAUNCHER_VERSION%
+
+    pause
+    goto menu
+) else if "%input%"=="4" (
+    exit /b 0
+) else (
+    echo Invalid choice.
+    pause
+)
+goto menu
+
+pause
